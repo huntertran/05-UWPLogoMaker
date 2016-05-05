89023fa327649f73622c380a1d892cc7cac2e294
Tuan Tran Van <cuoilennaocacban2@hotmail.com>
2016-05-05 23:08:35 +0700

    change language inside the app

--- UWPLogoMaker/View/SettingGroup/LanguagePage.xaml.cs
+++ UWPLogoMaker/View/SettingGroup/LanguagePage.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using Windows.Globalization;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using UWPLogoMaker.View.StartGroup;
 
 namespace UWPLogoMaker.View.SettingGroup
 {
@@ -17,6 +20,25 @@ namespace UWPLogoMaker.View.SettingGroup
         private void LanguagePage_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
             //ApplicationLanguages.
+        }
+
+        private void English_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            UpdateLanguage("en-US");
+        }
+
+
+        private void Vietnamese_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            UpdateLanguage("vi-VN");
+        }
+
+        private void UpdateLanguage(string language)
+        {
+            ApplicationLanguages.PrimaryLanguageOverride = language;
+            //Frame.Navigate(this.GetType());
+            Frame rootFrame = Window.Current.Content as Frame;
+            rootFrame.Navigate(typeof(StartPage));
         }
     }
 }
