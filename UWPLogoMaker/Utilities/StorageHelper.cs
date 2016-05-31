2205e68d4dfe63638fafd85d9999e050210aca0c
Tuan Tran Van <cuoilennaocacban2@hotmail.com>
2016-05-31 17:07:12 +0700

    go mobile

--- UWPLogoMaker/Utilities/StorageHelper.cs
+++ UWPLogoMaker/Utilities/StorageHelper.cs
@@ -10,14 +10,65 @@ namespace UWPLogoMaker.Utilities
 {
     public class StorageHelper
     {
+
+        #region Check exist
+
+        /// <summary>
+        /// Check if a folder is existed with its path
+        /// </summary>
+        /// <param name="path">Path to folder</param>
+        /// <returns></returns>
+        public static bool IsFolderExisted(string path)
+        {
+            return Directory.Exists(path);
+        }
+
+        /// <summary>
+        /// Check if a file is existed with its path
+        /// </summary>
+        /// <param name="fileName">path to file, not include Local folder root</param>
+        /// <param name="folder">Folder to check, if null, get Local Folder</param>
+        /// <param name="isCheckFileContent"></param>
+        /// <returns></returns>
+        public static bool IsFileExisted(string fileName, StorageFolder folder = null, bool isCheckFileContent = false)
+        {
+            bool result = false;
+            if (folder == null)
+            {
+                folder = ApplicationData.Current.LocalFolder;
+            }
+
+            if (!string.IsNullOrEmpty(fileName) && File.Exists(string.Concat(folder.Path, "\\", fileName)))
+            {
+                if (isCheckFileContent)
+                {
+                    var info = new FileInfo(folder.Path + "\\" + fileName);
+                    if (info.Length > 0)
+                    {
+                        result = true;
+                    }
+                }
+                else
+                {
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+
+        #endregion
+
         public static async Task<T> Json2Object<T>(string fileName, StorageFolder folder = null)
         {
-            try
+            if (folder == null)
             {
-                if (folder == null)
-                {
-                    folder = ApplicationData.Current.RoamingFolder;
-                }
+                folder = ApplicationData.Current.RoamingFolder;
+            }
+
+            if (IsFileExisted(fileName, folder))
+            {
+
                 StorageFile file = await folder.GetFileAsync(fileName);
                 using (Stream x = await file.OpenStreamForReadAsync())
                 {
@@ -27,10 +78,6 @@ namespace UWPLogoMaker.Utilities
                     T data = jObject.ToObject<T>();
                     return data;
                 }
-            }
-            catch
-            {
-                //add some code here
             }
             return default(T);
         }
