7f5dba0430b9672fac42e281d8c35b2c48968e52
Tuan Tran Van <tuan.tran@audiencemedia.com>
2017-01-13 18:05:46 +0700

    change to webview

--- UWPLogoMaker/View/SettingGroup/AboutPage.xaml.cs
+++ UWPLogoMaker/View/SettingGroup/AboutPage.xaml.cs
@@ -1,74 +1,10 @@
-﻿using System;
-using Windows.System;
-using Windows.UI.Xaml;
-
-namespace UWPLogoMaker.View.SettingGroup
+﻿namespace UWPLogoMaker.View.SettingGroup
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class AboutPage
     {
-        //private readonly DispatcherTimer _timer;
-        //private bool _isStarted;
-        //private int _times;
-
         public AboutPage()
         {
             InitializeComponent();
-            //_timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(5000) };
-            //_timer.Tick += _timer_Tick;
-            //_times = 0;
-        }
-
-        //private void _timer_Tick(object sender, object e)
-        //{
-        //    _times = 0;
-        //    _isStarted = false;
-        //    _timer.Stop();
-        //}
-
-        //private void Path_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
-        //{
-        //    if (!_isStarted)
-        //    {
-        //        _isStarted = true;
-        //        _timer.Start();
-        //    }
-        //    else
-        //    {
-        //        _times++;
-        //    }
-
-        //    if (_times >= 5)
-        //    {
-        //        TuanPic.Visibility = Visibility.Visible;
-        //        ThiPic.Visibility = Visibility.Visible;
-        //    }
-        //}
-
-        private async void EmailThi_OnClick(object sender, RoutedEventArgs e)
-        {
-            await
-                Launcher.LaunchUriAsync(
-                    new Uri("mailto:Thi.NguyenVuHoang@studentpartner.com?subject=[LockBe]&body=Hello, "));
-        }
-
-        private async void LinkedInThi_OnClick(object sender, RoutedEventArgs e)
-        {
-            await Launcher.LaunchUriAsync(new Uri("https://vn.linkedin.com/in/hoangthi1710"));
-        }
-
-        private async void EmailTuan_OnClick(object sender, RoutedEventArgs e)
-        {
-            await
-                Launcher.LaunchUriAsync(
-                    new Uri("mailto:cuoilennaocacban@hotmail.com?subject=[UWPLogoMaker]&body=Hello, "));
-        }
-
-        private async void LinkedInTuan_OnClick(object sender, RoutedEventArgs e)
-        {
-            await Launcher.LaunchUriAsync(new Uri("https://vn.linkedin.com/in/tuanmsp"));
         }
     }
 }
--- UWPLogoMaker/View/SettingGroup/MoreAppPage.xaml.cs
+++ UWPLogoMaker/View/SettingGroup/MoreAppPage.xaml.cs
@@ -1,29 +1,10 @@
-﻿using System;
-using System.Linq;
-using Windows.System;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Input;
-using UWPLogoMaker.Model;
-using UWPLogoMaker.ViewModel.SettingGroup;
-
-namespace UWPLogoMaker.View.SettingGroup
+﻿namespace UWPLogoMaker.View.SettingGroup
 {
     public sealed partial class MoreAppPage
     {
-        public MoreAppViewModel Vm => (MoreAppViewModel)DataContext;
-
         public MoreAppPage()
         {
             InitializeComponent();
-        }
-
-        public async void MoreAppItem_OnTapped(object sender, TappedRoutedEventArgs e)
-        {
-            AppItem a = ((Grid)sender).DataContext as AppItem;
-            if (a != null)
-                await
-                    Launcher.LaunchUriAsync(
-                        new Uri("ms-windows-store://pdp/?ProductId=" + a.link.Trim('/').Split('/').Last()));
         }
     }
 }
