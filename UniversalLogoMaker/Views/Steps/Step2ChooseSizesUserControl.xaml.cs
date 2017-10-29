ee699f868b1dcd317071e1bb66de889788fc8dd1
Tuan Tran Van <cuoilennaocacban2@hotmail.com>
2017-10-29 17:26:52 +0700

    step 2 choose sizes

---
+++ UniversalLogoMaker/Views/Steps/Step2ChooseSizesUserControl.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace UniversalLogoMaker.Views.Steps
+{
+    using ViewModels;
+
+    public sealed partial class Step2ChooseSizesUserControl
+    {
+        private StartViewModel _vm;
+
+        public Step2ChooseSizesUserControl()
+        {
+            _vm = (StartViewModel)DataContext;
+
+            InitializeComponent();
+        }
+    }
+}
