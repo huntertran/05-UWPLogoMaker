1e6bc503d519cf10f4557bc87cd36c97c64722ee
Tuan Tran Van <cuoilennaocacban2@hotmail.com>
2018-02-21 11:42:05 +0700

    add version 3

---
+++ UniversalLogoMaker3/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using UniversalLogoMaker3.Helpers;
+
+namespace UniversalLogoMaker3.ViewModels
+{
+    public class MainViewModel : Observable
+    {
+        public MainViewModel()
+        {
+        }
+    }
+}
---
+++ UniversalLogoMaker3/Views/AboutPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using UniversalLogoMaker3.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace UniversalLogoMaker3.Views
+{
+    public sealed partial class AboutPage : Page
+    {
+        public AboutViewModel ViewModel { get; } = new AboutViewModel();
+
+        public AboutPage()
+        {
+            InitializeComponent();
+            ViewModel.Initialize(webView);
+        }
+    }
+}
