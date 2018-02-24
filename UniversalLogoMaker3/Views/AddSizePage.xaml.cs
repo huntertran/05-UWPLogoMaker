cfb164be371793211fcdbdba33cd36b4ceb26545
Tuan Tran Van <cuoilennaocacban2@hotmail.com>
2018-02-24 23:06:12 +0700

    add size success

--- UniversalLogoMaker3/Views/AddSizePage.xaml.cs
+++ UniversalLogoMaker3/Views/AddSizePage.xaml.cs
@@ -1,6 +1,11 @@
 ﻿namespace UniversalLogoMaker3.Views
 {
+    using Models;
+    using Services;
     using ViewModels;
+    using Windows.UI.Popups;
+    using Windows.UI.Xaml.Controls;
+    using Windows.UI.Xaml.Input;
 
     public sealed partial class AddSizePage
     {
@@ -9,6 +14,37 @@
         public AddSizePage()
         {
             InitializeComponent();
+            Loaded += AddSizePage_Loaded;
+        }
+
+        private async void AddSizePage_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            await ViewModel.LoadData();
+        }
+
+        private async void DeletedPlatform_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            Platform p = ((Button)sender).Tag as Platform;
+            ViewModel.CustomData.PlatformList.Remove(p);
+            await StorageHelper.Object2Json(ViewModel.CustomData, "custom.dat");
+        }
+
+        private async void AddButton_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(PlatformNameTextBox.Text) || string.IsNullOrEmpty(PlatformShortNameTextBox.Text))
+            {
+                MessageDialog msg = new MessageDialog("Please enter platform name and shortname", "Error");
+                msg.ShowAsync();
+            }
+            else
+            {
+                await ViewModel.AddNewPlatform(PlatformNameTextBox.Text, PlatformShortNameTextBox.Text, SizeTextBox.Text);
+            }
+        }
+
+        private void SizeTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ViewModel.ParseLogoObject(SizeTextBox.Text);
         }
     }
 }
