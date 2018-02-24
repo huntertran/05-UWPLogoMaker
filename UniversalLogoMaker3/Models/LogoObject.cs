9811f6be3d649d8be2cb684846322753c7a6a086
Tuan Tran Van <cuoilennaocacban2@hotmail.com>
2018-02-24 19:40:19 +0700

    AddSizePage - Brush in xaml not working

---
+++ UniversalLogoMaker3/Models/LogoObject.cs
@@ -0,0 +1,93 @@
+﻿namespace UniversalLogoMaker3.Models
+{
+    using System;
+    using Helpers;
+
+    public class LogoObject : Observable
+    {
+        private string _fileName;
+        private int _width, _height, _scale;
+
+        public int Width
+        {
+            get => _width;
+            set
+            {
+                if (value > 0)
+                {
+                    if (value == _width) return;
+                    _width = value;
+                    OnPropertyChanged(nameof(Width));
+                }
+            }
+        }
+
+        public int Height
+        {
+            get => _height;
+            set
+            {
+                if (value > 0)
+                {
+                    if (value == _height) return;
+                    _height = value;
+                    OnPropertyChanged(nameof(Height));
+                }
+            }
+        }
+
+        public int Scale
+        {
+            get => _scale;
+            set
+            {
+                if (value > 0)
+                {
+                    if (value == _scale) return;
+                    _scale = value;
+                    OnPropertyChanged(nameof(Scale));
+                }
+            }
+        }
+
+        public string FileName
+        {
+            get => _fileName;
+            set => Set(ref _fileName, value);
+        }
+
+        /// <summary>
+        /// Default Constructor, required for Json serialization
+        /// </summary>
+        public LogoObject()
+        {
+
+        }
+
+        public LogoObject(string name, int widthSize, int height)
+        {
+            FileName = name;
+            Scale = 100;
+            Width = widthSize;
+            Height = height;
+        }
+
+        public LogoObject(string name, int scale, int widthSize, bool isSquare, string ratio = "310:150")
+        {
+            FileName = name;
+            Scale = scale;
+            Width = (int)Math.Ceiling((double)(widthSize * scale) / 100);
+            if (isSquare)
+            {
+                Height = (int)Math.Ceiling((double)(widthSize * scale) / 100);
+            }
+            else
+            {
+                int upLeft = Convert.ToInt32(ratio.Split(':')[0]);
+                int downLeft = Convert.ToInt32(ratio.Split(':')[1]);
+
+                Height = (int)Math.Ceiling((double)(widthSize * downLeft / upLeft * scale) / 100);
+            }
+        }
+    }
+}
--- UniversalLogoMaker3/ViewModels/AddSizeViewModel.cs
+++ UniversalLogoMaker3/ViewModels/AddSizeViewModel.cs
@@ -1,13 +1,87 @@
-﻿using System;
-
-using UniversalLogoMaker3.Helpers;
-
-namespace UniversalLogoMaker3.ViewModels
+﻿namespace UniversalLogoMaker3.ViewModels
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Threading.Tasks;
+    using Helpers;
+    using Models;
+    using Services;
+
     public class AddSizeViewModel : Observable
     {
-        public AddSizeViewModel()
+        private ObservableCollection<LogoObject> _logoObjectList;
+        private Database _customData;
+
+        public ObservableCollection<LogoObject> LogoObjectList
         {
+            get => _logoObjectList;
+            set => Set(ref _logoObjectList, value);
+        }
+
+        public Database CustomData
+        {
+            get => _customData;
+            set => Set(ref _customData, value);
+        }
+
+        public async Task LoadData()
+        {
+            CustomData = await StorageHelper.Json2Object<Database>("custom.dat") ?? new Database
+            {
+                PlatformList = new ObservableCollection<Platform>()
+            };
+        }
+
+        public async Task AddNewPlatform(string platformName, string platformAbbreviation, string size)
+        {
+            Platform p = new Platform
+            {
+                Name = platformName,
+                Icon = platformAbbreviation,
+                SaveLogoList = new List<LogoObject>()
+            };
+
+            string[] text = size.Split(';');
+            foreach (string s in text)
+            {
+                if (!string.IsNullOrEmpty(s))
+                {
+                    string[] t = s.Trim().Split(':');
+                    LogoObject l = new LogoObject(t[0], Convert.ToInt32(t[1]), Convert.ToInt32(t[2]));
+                    p.SaveLogoList.Add(l);
+                }
+            }
+
+            CustomData.PlatformList.Add(p);
+            //Save all change to file
+            await StorageHelper.Object2Json(CustomData, "custom.dat");
+        }
+
+        public void ParseLogoObject(string data)
+        {
+            if (LogoObjectList == null)
+            {
+                LogoObjectList = new ObservableCollection<LogoObject>();
+            }
+            LogoObjectList.Clear();
+            string[] text = data.Split(';');
+            foreach (string s in text)
+            {
+                try
+                {
+                    if (!string.IsNullOrEmpty(s))
+                    {
+                        string[] t = s.Trim().Split(':');
+                        LogoObject l = new LogoObject(t[0], Convert.ToInt32(t[1]), Convert.ToInt32(t[2]));
+                        LogoObjectList.Add(l);
+                    }
+                }
+                catch
+                {
+                    // ignored
+                }
+            }
         }
     }
 }
